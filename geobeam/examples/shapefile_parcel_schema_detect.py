659b1c4b4274bb8cc8c70bdbf34dcd2e37a3571d
Vadim Zaripov <vadimzaripov@google.com>
2022-10-17 23:42:02 +0100

    Autoschema

--- geobeam/examples/shapefile_parcel_schema_detect.py
+++ geobeam/examples/shapefile_parcel_schema_detect.py
@@ -17,6 +17,7 @@ Example pipeline that loads a county parcel shape dataset into BigQuery.
 """
 
 import apache_beam as beam
+from google.cloud import bigquery
 import geobeam
 from apache_beam.io.gcp.internal.clients import bigquery as beam_bigquery
 from apache_beam.io.gcp.bigquery_tools import parse_table_schema_from_json
@@ -60,65 +61,80 @@ def typecast_fields(record):
 
 '''
     
-def get_schema(known_args):
-
-    
-    gcs_url = known_args.gcs_url
-    bucket_name = gcs_url.split('/')[2]
-    file_name = '/'.join(gcs_url.split('/')[3:])
-    zip_name = gcs_url.split('/')[-1].split('.')[0]
+ 
+gcs_url = known_args.gcs_url
+bucket_name = gcs_url.split('/')[2]
+file_name = '/'.join(gcs_url.split('/')[3:])
+zip_name = gcs_url.split('/')[-1].split('.')[0]
   
-    storage_client = storage.Client()
-    blob = storage_client.bucket(bucket_name).get_blob(file_name)
-    source_bucket = storage_client.bucket(bucket_name)
-    blob_uri = gcs_url
+storage_client = storage.Client()
+blob = storage_client.bucket(bucket_name).get_blob(file_name)
+source_bucket = storage_client.bucket(bucket_name)
+blob_uri = gcs_url
 
-    blob_2 = source_bucket.blob(file_name)
-    data = blob.download_as_string()
+blob_2 = source_bucket.blob(file_name)
+data = blob.download_as_string()
     
-    profile = None
-    layer_name= known_args.layer_name
+profile = None
+layer_name= known_args.layer_name
 
-    if layer_name is not None:
-        with fiona.io.ZipMemoryFile(data) as zip:
-            with zip.open(f'{zip_name}.shp',layer_name) as collection:
-                print(collection)
-                profile = collection.profile
-    elif layer_name is not None:
-        profile = BytesCollection(data, layer=layer_name).profile
-    else:
-        profile = fiona.open(gcs_url).profile
+if layer_name is not None:
+    with fiona.io.ZipMemoryFile(data) as zip:
+        with zip.open(f'{zip_name}.shp',layer_name) as collection:
+            print(collection)
+            profile = collection.profile
+elif layer_name is not None:
+    profile = BytesCollection(data, layer=layer_name).profile
+else:
+    profile = fiona.open(gcs_url).profile
             
-    from fiona import prop_type
+from fiona import prop_type
 
-    BQ_FIELD_TYPES = {
-        'int': 'INTEGER',
-        'str': 'STRING',
-        'float': 'FLOAT',
-        'bool': 'BOOL',
-        'date': 'DATE',
-        'time': 'TIME',
-        'datetime': 'DATETIME',
-        'bytes': 'BYTES'
-    }
+BQ_FIELD_TYPES = {
+    'int': 'INTEGER',
+    'str': 'STRING',
+    'float': 'FLOAT',
+    'bool': 'BOOL',
+    'date': 'DATE',
+    'time': 'TIME',
+    'datetime': 'DATETIME',
+    'bytes': 'BYTES'
+}
 
-    bq_schema = []
+bq_schema = []
 
-    for field_name, field_type in profile['schema']['properties'].items():
-        fiona_type = prop_type(field_type)
-        bq_type = BQ_FIELD_TYPES[fiona.schema.FIELD_TYPES_MAP_REV[fiona_type]]
-        bq_schema.append({
-            'name': field_name,
-            'type': bq_type
-        })
-
+for field_name, field_type in profile['schema']['properties'].items():
+    fiona_type = prop_type(field_type)
+    bq_type = BQ_FIELD_TYPES[fiona.schema.FIELD_TYPES_MAP_REV[fiona_type]]
     bq_schema.append({
-        'name': 'geom',
-        'type': 'GEOGRAPHY',
-           })
+        'name': field_name,
+        'type': bq_type
+    })
+
+bq_schema.append({
+    'name': 'geom',
+    'type': 'GEOGRAPHY',
+        })
     
-    return json.JSONEncoder(sort_keys=True).encode({"fields": bq_schema})
-    #return bq_schema
+schema_json = json.JSONEncoder(sort_keys=True).encode(bq_schema)
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+# TODO(developer): Set table_id to the ID of the table to create.
+table_id = format(known_args.project, known_args.dataset, known_args.table)
+
+bigquerySchema = []
+
+bigqueryColumns = json.loads(schema_json)
+for col in bigqueryColumns:
+    bigquerySchema.append(bigquery.SchemaField(col['name'], col['type']))
+
+table = bigquery.Table(table_id, schema=bigquerySchema)
+table = client.create_table(table)  # Make an API request.
+print(
+    "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
+)
 
 
 def run(pipeline_args, known_args):
@@ -133,18 +149,16 @@ def run(pipeline_args, known_args):
         (p
          | beam.io.Read(ShapefileSource(known_args.gcs_url,
              layer_name=known_args.layer_name))
-         #| 'MakeValid' >> beam.Map(make_valid)
+         | 'MakeValid' >> beam.Map(make_valid)
          | 'FilterInvalid' >> beam.Filter(filter_invalid)
          | 'FormatRecords' >> beam.Map(format_record)
          | 'WriteToBigQuery' >> beam.io.WriteToBigQuery(
              beam_bigquery.TableReference(
                  datasetId=known_args.dataset,
                  tableId=known_args.table),
-             schema=get_schema(known_args),
-             #schema='SCHEMA_AUTODETECT',
              method=beam.io.WriteToBigQuery.Method.FILE_LOADS,
              write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
-             create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED))
+             create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER))
              
 
 
@@ -158,6 +172,7 @@ if __name__ == '__main__':
     parser.add_argument('--gcs_url')
     parser.add_argument('--dataset')
     parser.add_argument('--table')
+    parser.add_argument('--project')
     parser.add_argument('--layer_name')
     parser.add_argument('--in_epsg', type=int, default=None)
     known_args, pipeline_args = parser.parse_known_args()
